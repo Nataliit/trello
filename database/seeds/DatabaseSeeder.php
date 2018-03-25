dfb98d03c5c3046aa9cda5cdc370dd79ed387519
Natali <maxwell141417@gmail.com>
2018-03-25 11:13:51 +0300

    create seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Desk;
 
 class DatabaseSeeder extends Seeder
 {
@@ -9,6 +10,29 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
+    public function generateUsers(){
+        $desk_title = [
+            'Правки',
+            'Установки',
+            'Новинки',
+            'Лучшее',
+        ];
+        $desk_description = [
+            'Все о правках',
+            'Все об установках',
+            'Все о новинках',
+            'Все о лучшем',
+        ];
+
+        foreach (array_combine($desk_title, $desk_description) as $name => $description){
+            Desk::create([
+                'name' => $name,
+                'description' => $description
+            ]);
+        }
+    }
+
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
