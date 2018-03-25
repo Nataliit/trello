a18b337f693819cc96ca565cdb05c4f8df4f9556
Natali <maxwell141417@gmail.com>
2018-03-25 10:31:59 +0300

    db settings

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,29 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('desks', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Desk::all();
+});
+
+Route::get('desks/{id}', function($id) {
+    return Desk::find($id);
+});
+
+Route::post('desks', function(Request $request) {
+    return Desk::create($request->all);
+});
+
+Route::put('desks/{id}', function(Request $request, $id) {
+    $article = Desk::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('desks/{id}', function($id) {
+    Desk::find($id)->delete();
+
+    return 204;
+});
