da146c4025f2b109faf34fbd99bb937402b8a1e6
Natali <maxwell141417@gmail.com>
2018-03-25 10:40:07 +0300

    creste desk controller and route

--- routes/api.php
+++ routes/api.php
@@ -13,32 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('desks', function() {
-    // If the Content-Type and Accept headers are set to 'application/json',
-    // this will return a JSON structure. This will be cleaned up later.
-    return Desk::all();
-});
-
-Route::get('desks/{id}', function($id) {
-    return Desk::find($id);
-});
-
-Route::post('desks', function(Request $request) {
-    return Desk::create($request->all);
-});
-
-Route::put('desks/{id}', function(Request $request, $id) {
-    $article = Desk::findOrFail($id);
-    $article->update($request->all());
-
-    return $article;
-});
-
-Route::delete('desks/{id}', function($id) {
-    Desk::find($id)->delete();
-
-    return 204;
-});
+Route::get('desks', 'DeskController@index');
+Route::get('desks/{desk}', 'DeskController@show');
+Route::post('desks', 'DeskController@store');
+Route::put('desks/{desk}', 'DeskController@update');
+Route::delete('desks/{desk}', 'DeskController@delete');
